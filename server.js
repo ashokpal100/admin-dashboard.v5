1b6f658772c815c50ee78a578586ea07db02412d
ashok pal <pala90651@gmail.com>
2018-02-15 17:54:45 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const appRoot = 'src';
+
+app.use(express.static(path.join(__dirname,appRoot)))
+
+app.get('/*',function(req,res,next){
+    res.sendfile('./'+appRoot+'/index.html');
+})
+
+
+post = 9000;
+
+app.listen(post, "0.0.0.0", () => console.log('Example app listening on port :'+post+"!"))
